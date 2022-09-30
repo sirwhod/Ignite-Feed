0f88e97ed37b04f181ee2fb599cb36b1faeb25b0
sirwhod <rodrigo.brandao98@gmail.com>
2022-09-30 15:21:26 -0300

    Realizando likes nos Comentários

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,8 +1,15 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 import styles from './Comment.module.css'
 
 export function Comment({content, onDeleteComment}) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
+  }
 
   function handleDeleteComment() {
 
@@ -29,9 +36,9 @@ export function Comment({content, onDeleteComment}) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment} >
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
